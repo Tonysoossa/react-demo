13d6b2f0cf38f94c4ce9539f63e63dcb159da6d2
Tonysoossa <tony.servanin@gmail.com>
2023-07-05 21:00:06 +0200

    header set on the page top

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "./app.css";
-
 import "./reset.css";
 import styles from "./app.module.css";
 import { ActionCard } from "./components/ActionCard";
@@ -7,51 +6,54 @@ import { ClickMe } from "./components/ClickMeCard";
 import { DeleteMessage } from "./components/DeleteMessage";
 import { useState } from "react";
 import { ResetButton } from "./components/ResetButton";
-
+import { UpperHead } from "./components/UpperHead";
 
 function App() {
   const [show, setShow] = useState<boolean>(false);
   const [showDeleteM, setShowDeleteM] = useState<boolean>(false);
   const [number, setNumber] = useState(0);
   return (
-    <div className={styles.container}>
-      <ResetButton
-        onResetClick={() => {
-          setShow(false);
-          setShowDeleteM(false);
-          setNumber(0);
-          console.log("reset");
-        }}
-        title="Reload page"
-      />
-      {!showDeleteM && (
-        <ClickMe
-          onClick={() => setShow(true)}
-          number={number}
-          setNumber={setNumber}
-          title="Tony"
+    <>
+      <UpperHead title="gang" />
+      <div className={styles.container}>
+        <ResetButton
+          onResetClick={() => {
+            setShow(false);
+            setShowDeleteM(false);
+            setNumber(0);
+            console.log("reset");
+          }}
+          title="Reload page"
         />
-      )}
-      {show && (
-        <ActionCard
-          onCancelClick={() => {
-            setShow(false);
-            console.log("You did click on Cancel!");
-          }}
-          onDeleteClick={() => {
-            setShow(false);
-            setShowDeleteM(true);
-            console.log("You did click on Delete!");
-          }}
-          title="Are you sure ?"
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-          enim.
-        </ActionCard>
-      )}
+        {!showDeleteM && (
+          <ClickMe
+            onClick={() => setShow(true)}
+            number={number}
+            setNumber={setNumber}
+            title="Tony"
+          />
+        )}
+        {show && (
+          <ActionCard
+            onCancelClick={() => {
+              setShow(false);
+              console.log("You did click on Cancel!");
+            }}
+            onDeleteClick={() => {
+              setShow(false);
+              setShowDeleteM(true);
+              console.log("You did click on Delete!");
+            }}
+            title="Are you sure ?"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+            enim.
+          </ActionCard>
+        )}
 
-      {showDeleteM && <DeleteMessage message="Delete completed !" />}
-    </div>
+        {showDeleteM && <DeleteMessage message="Delete completed !" />}
+      </div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-
 import "./index.css";
-import { UpperHead } from "./components/UpperHead/UpperHead.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <UpperHead title="gang" />
     <App />
   </React.StrictMode>
 );
-
-// import upperStyles from "./app.css";
-// <div>
-//   <UpperHead className={upperStyles.logo}></UpperHead>
-// </div>

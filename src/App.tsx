a05eaf88f3ce36a5e01d97f0584e744e41c03d31
Tonysoossa <tony.servanin@gmail.com>
2023-07-07 12:27:28 +0200

    8mort, 6 blesser, on pète la bierre, ma libulul!

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { DeleteMessage } from "./components/DeleteMessage";
 import { useState } from "react";
 import { ResetButton } from "./components/ResetButton";
 import { UpperHead } from "./components/UpperHead";
+import { Footer } from "./components/Footer";
 
 function App() {
   const [show, setShow] = useState<boolean>(false);
@@ -16,42 +17,45 @@ function App() {
     <>
       <UpperHead title="this is a nav bar" />
       <div className={styles.container}>
-        {!showDeleteM && (
-          <ClickMe
-            onClick={() => setShow(true)}
-            number={number}
-            setNumber={setNumber}
-            title="Tony"
+        <div className={styles.centerDiv}>
+          {!showDeleteM && (
+            <ClickMe
+              onClick={() => setShow(true)}
+              number={number}
+              setNumber={setNumber}
+              title="Tony"
+            />
+          )}
+          {show && (
+            <ActionCard
+              onCancelClick={() => {
+                setShow(false);
+                console.log("You did click on Cancel!");
+              }}
+              onDeleteClick={() => {
+                setShow(false);
+                setShowDeleteM(true);
+                console.log("You did click on Delete!");
+              }}
+              title="Are you sure ?"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              enim.
+            </ActionCard>
+          )}
+          {showDeleteM && <DeleteMessage message="Delete completed !" />}
+          <ResetButton
+            onResetClick={() => {
+              setShow(false);
+              setShowDeleteM(false);
+              setNumber(0);
+              console.log("reset");
+            }}
+            title="Reload page"
           />
-        )}
-        {show && (
-          <ActionCard
-            onCancelClick={() => {
-              setShow(false);
-              console.log("You did click on Cancel!");
-            }}
-            onDeleteClick={() => {
-              setShow(false);
-              setShowDeleteM(true);
-              console.log("You did click on Delete!");
-            }}
-            title="Are you sure ?"
-          >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-            enim.
-          </ActionCard>
-        )}
-        {showDeleteM && <DeleteMessage message="Delete completed !" />}
-        <ResetButton
-          onResetClick={() => {
-            setShow(false);
-            setShowDeleteM(false);
-            setNumber(0);
-            console.log("reset");
-          }}
-          title="Reload page"
-        />
+        </div>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./footer.module.css";
+
+export function Footer() {
+  return (
+    <div className={styles.footer}>
+      <a className={styles.link} href="https://github.com/Tonysoossa">
+        Visit Github:
+      </a>
+    </div>
+  );
+}

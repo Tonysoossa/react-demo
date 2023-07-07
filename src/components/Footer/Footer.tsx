02010812bb4c863111a068e44016d45bf01056d5
Tonysoossa <tony.servanin@gmail.com>
2023-07-07 12:29:48 +0200

    j'avais oublier un truc zeubi

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,11 @@ import styles from "./footer.module.css";
 export function Footer() {
   return (
     <div className={styles.footer}>
-      <a className={styles.link} href="https://github.com/Tonysoossa">
+      <a
+        className={styles.link}
+        href="https://github.com/Tonysoossa"
+        target="blank"
+      >
         Visit Github:
       </a>
     </div>
